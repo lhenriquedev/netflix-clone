285f6054d4622bb72afd5151c9a5b971a2405161
lhenrique.dev <lhenrique.dev@gmail.com>
2022-04-06 16:22:51 -0300

    :rocket: resolving node-sass dependecy

--- src/components/Posters/index.js
+++ src/components/Posters/index.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../../services/axios';
+import React, { useState, useEffect } from "react";
+import axios from "../../services/axios";
 
-import Youtube from 'react-youtube';
-import MovieTrailer from 'movie-trailer';
+import Youtube from "react-youtube";
+import MovieTrailer from "movie-trailer";
 
-import './style.scss';
+import "./style.scss";
 
-const base_image_url = 'https://image.tmdb.org/t/p/original/';
+const base_image_url = "https://image.tmdb.org/t/p/original/";
 
 function Posters({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState('');
+  const [trailerUrl, setTrailerUrl] = useState("");
   // A snippet of code which runs based on a specific condition/variable;
   useEffect(() => {
     async function fetchData() {
@@ -23,8 +23,8 @@ function Posters({ title, fetchUrl, isLargeRow }) {
   }, [fetchUrl]);
 
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
@@ -33,14 +33,14 @@ function Posters({ title, fetchUrl, isLargeRow }) {
   // if [], run once when the row loads, and dont run again
   const handleClick = (movie) => {
     if (trailerUrl) {
-      setTrailerUrl('');
+      setTrailerUrl("");
     } else {
-      MovieTrailer(movie?.name || '')
+      MovieTrailer(movie?.name || "")
         .then((url) => {
           // https://www.youtube.com/watch?v=XtMThy8QKqU
           // get all params after 'v'
           const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get('v'));
+          setTrailerUrl(urlParams.get("v"));
         })
         .catch((err) => console.log(err));
     }
@@ -54,11 +54,9 @@ function Posters({ title, fetchUrl, isLargeRow }) {
         {/* posters */}
         {movies.map((movie) => (
           <img
-            className={`posters__image ${isLargeRow && 'posters__imageLarge'}`}
+            className={`posters__image ${isLargeRow && "posters__imageLarge"}`}
             onClick={() => handleClick(movie)}
-            src={`${base_image_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
+            src={`${base_image_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
             key={movie.id}
           />
